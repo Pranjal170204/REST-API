c29dba1e259453fb46379473fe9c6523125fdc45
Pranjal170204 <pranjal.srivastava@wohlig.com>
2025-02-14 11:18:59 +0530

    Tested http methods

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const app=express()
 const PORT=8000;
+const fs=require('fs')
 const users=require('./MOCK_DATA.json')
 //ROUTES
-
+app.use(express.urlencoded({extended:false}))
 
 app.get('/api/users',(req,res)=>{
     return res.json(users)
@@ -32,15 +33,47 @@ app.get('/api/users/:id',(req,res)=>{
 
 })
 
-app.route("./api/users/:id")
-.post((req,res)=>{
-    res.json({status:"pending"})
+app.route('/api/users').post((req,res)=>{
+    const body=req.body;
+    console.log(body);
+    users.push({id:users.length+1,...body});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users,null,2),(err,data)=>{
+        return res.json({status:"added new user"}); 
+    })
+
 })
-.patch((req,res)=>{
+
+app.route('/api/users/:id').patch((req,res)=>{
+    const user_id=parseInt(req.params.id)
+    const body=req.body;
+   
+    console.log(body);
+    const userIndex=users.findIndex(user=>{
+       return user.id===user_id
+    })
+    if(userIndex===-1) return res.status(404).json({status:"error",message:"User not found"});
+
+    users[userIndex]={...users[userIndex],...body}
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users,null,2),(err,data)=>{
+        console.log(data);
+    })
+
 
 })
 .delete((req,res)=>{
-    
+    const user_id=parseInt(req.params.id)
+    const userIndex=users.findIndex(user=>{
+        return user.id===user_id
+    })
+    if(userIndex===-1) return res.status(404).json({status:"error",message:"User not found"});
+
+    users.splice(userIndex,1);
+
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users,null,2),(err,data)=>{
+        res.send("User deleted");
+    })
+
+
 })
 
 app.listen(PORT ,()=>{
